af745db4870c6bde3b5e0a580478658fd557168b
Ste79ITA <nastri.s@gmail.com>
2023-10-25 15:31:05 +0200

    folder setup

---
+++ js/app.js
@@ -0,0 +1,14 @@
+let todos = [
+  {
+    text: 'Fare i compiti',
+    done: false,
+  },
+  {
+    text: 'Fare la spesa',
+    done: true,
+  },
+  {
+    text: 'Fare il bucato',
+    done: false,
+  },
+];
